ea4d99a64628ba1ad6a34635167902be253f6e73
Boublet <65109951+Boublet@users.noreply.github.com>
2023-05-24 15:21:54 +0700

    Xóa mấy cái thư viện lol

--- Chess/MenuScreen.cpp
+++ Chess/MenuScreen.cpp
@@ -1,5 +1,8 @@
 ﻿#include "MenuScreen.h"
 
 MenuScreen::MenuScreen(float windowWidthScale, float windowHeightScale, RenderGame* render, GameState* game) : Screen(windowWidthScale, windowHeightScale, render, game) {}
-void MenuScreen::run(RenderWindow& window, Screen* screen) {}
+void MenuScreen::run(RenderWindow& window, Screen* screen) {
+
+
+}
   
